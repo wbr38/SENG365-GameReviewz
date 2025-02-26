38a695ac8b2226a85326dd14c6f2d92914f9bc2a
William Brown <wbr38@uclive.ac.nz>
2025-02-26 19:22:47 +1300

    [Api] tsconfig: set strict: true and enable incremental builds

--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -4,18 +4,20 @@ import Logger from "./logger";
 dotenv.config();
 
 const state: { pool: mysql.Pool} = {
-    pool: null
+    pool: null as unknown as mysql.Pool 
 }
 
 const connect = async (): Promise<void> => {
-    state.pool = mysql.createPool( {
+    const port = process.env.SENG365_MYSQL_PORT ? parseInt(process.env.SENG365_MYSQL_PORT, 10) : 3306;
+
+    state.pool = await mysql.createPool({
         connectionLimit: 100,
         multipleStatements: true,
         host: process.env.SENG365_MYSQL_HOST,
         user: process.env.SENG365_MYSQL_USER,
         password: process.env.SENG365_MYSQL_PASSWORD,
         database: process.env.SENG365_MYSQL_DATABASE,
-        port: parseInt(process.env.SENG365_MYSQL_PORT,10) || 3306,
+        port,
         ssl: {
             rejectUnauthorized: false
         }
